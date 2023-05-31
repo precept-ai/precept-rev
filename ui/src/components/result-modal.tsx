0145a13205ccbb08deacdd1b042269cd801ba887
Rayhan Beebeejaun <44209174+rayhanrsb@users.noreply.github.com>
2023-05-31 15:23:41 +0100

    vertical popup layout

--- ui/src/components/result-modal.tsx
+++ ui/src/components/result-modal.tsx
@@ -17,58 +17,82 @@ export const ResultModal = (props: ResultModalProps) => {
         onClick={props.closeModal}
       ></div>
       <div
-        className="w-4/5 h-[75vh] flex flex-row justify-center items-start p-[20px] gap-[30px] z-20 bg-[#fff] rounded-[10px]"
+        className="w-4/5 h-[85vh] flex flex-col justify-center items-center p-[20px] z-20 bg-[#fff] rounded-[10px]"
         onClick={() => {}}
       >
-        <div className="w-[144px] h-[144px]">
-          {getBigIcon({
-            resultDetails: props.result,
-            dataSourceType: props.dataSourceType,
-          })}
-        </div>
-        <div className="w-full h-full flex flex-col items-start justify-between p-[10px] gap-[10px]">
-          <h2 className="text-2xl text-[#0D7E97] font-bold font-dm-sans">
-            {props.result.title}
-          </h2>
-          <div className="w-full h-full flex flex-col items-start gap-[10px] overflow-auto">
-            {convertToGroupsOfTwo(props.result.content).map(
-              (groupOfTwo, index) => (
-                <div>
-                  {groupOfTwo.map((text_part, index) => (
-                    <span
-                      key={index}
-                      className={
-                        (text_part.bold ? "font-bold text-black" : "") +
-                        " text-md font-dm-sans font-regular"
-                      }
-                    >
-                      {text_part.content}
-                    </span>
-                  ))}
-                </div>
-              )
-            )}
+        <div className="w-full flex flex-row items-start p-[10px] gap-[20px]">
+          <div className="">
+            {getBigIcon({
+              resultDetails: props.result,
+              dataSourceType: props.dataSourceType,
+            })}
           </div>
-          <div className="w-full flex flex-row items-end justify-between p-[10px] gap-[10px]">
-            <a
-              href={props.result.url}
-              target="_blank"
-              rel="noreferrer"
-              className="w-full max-w-[250px] no-underline color text-white cursor-pointer"
-            >
-              <button className="w-full max-w-[250px] h-[45px] bg-[#0d7e97] text-white font-dm-sans font-bold rounded-[10px] cursor-pointer border-none outline-none">
-                Open
-              </button>
-            </a>
-            <a
-              className="text-black font-dm-sans"
-              href={getDownloadUrl(props.result)}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Download
-            </a>
+          <div className="w-full h-full flex flex-col items-start justify-between p-[10px] gap-[10px]">
+            <h2 className="text-2xl text-[#0D7E97] font-bold font-dm-sans">
+              {props.result.title}
+            </h2>
+            <div className="w-full h-full overflow-auto">
+              <span
+                className={
+                  (props.result.content[0].bold ? "font-bold text-black" : "") +
+                  " text-md font-dm-sans font-regular"
+                }
+              >
+                {props.result.content[0].content}
+              </span>
+              <span
+                className={
+                  (props.result.content[1].bold ? "font-bold text-black" : "") +
+                  " text-md font-dm-sans font-regular"
+                }
+              >
+                {props.result.content[1].content}
+              </span>
+              {/* {convertToGroupsOfTwo(props.result.content).map(
+                (groupOfTwo, index) => (
+                  <div>
+                    {groupOfTwo.map((text_part, index) => (
+                      <span
+                        key={index}
+                        className={
+                          (text_part.bold ? "font-bold text-black" : "") +
+                          " text-md font-dm-sans font-regular"
+                        }
+                      >
+                        {text_part.content}
+                      </span>
+                    ))}
+                  </div>
+                )
+              )} */}
+            </div>
+            <div className="w-full flex flex-row items-center p-[10px] gap-[40px]">
+              <a
+                href={props.result.url}
+                target="_blank"
+                rel="noreferrer"
+                className="w-full max-w-[250px] no-underline color text-white cursor-pointer"
+              >
+                <button className="w-full max-w-[250px] h-[45px] bg-[#0d7e97] text-white font-dm-sans font-bold rounded-[10px] cursor-pointer border-none outline-none">
+                  Open
+                </button>
+              </a>
+              <a
+                className="text-black font-dm-sans"
+                href={getDownloadUrl(props.result)}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Download
+              </a>
+            </div>
           </div>
+          <button
+            onClick={props.closeModal}
+            className="text-2xl text-red-600 font-dm-sans font-bold"
+          >
+            &times;
+          </button>
         </div>
         <iframe
           className="w-full h-full"
@@ -77,12 +101,6 @@ export const ResultModal = (props: ResultModalProps) => {
           allowFullScreen={true}
           title="Document Preview"
         />
-        <button
-          onClick={props.closeModal}
-          className="text-2xl text-red-600 font-dm-sans font-bold"
-        >
-          &times;
-        </button>
       </div>
     </div>
   );
