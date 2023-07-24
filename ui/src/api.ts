9c10fc40600f9481c22a0c2fcee19313008da8e8
Rayhan Beebeejaun <44209174+rayhanrsb@users.noreply.github.com>
2023-07-24 12:59:11 +0100

    use production api port in app

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-// let port = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 8000 : window.location.port;
+// let port =
+//   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+//     ? 8000
+//     : window.location.port;
 // export const api = axios.create({
 //   baseURL: `${window.location.protocol}//${window.location.hostname}:${port}/api/v1`,
-// })
+// });
+
 const port = "https://34.163.248.187";
 export const api = axios.create({
   baseURL: `${port}/api/v1`,
